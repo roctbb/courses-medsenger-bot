ccd08c619bcb9f2e2766117ada1db3e2541f194e
roctbb <roctbb@gmail.com>
2023-07-28 11:29:56 +0300

    add debug

--- courses_jobs.py
+++ courses_jobs.py
@@ -2,6 +2,8 @@ from courses_bot import *
 from logic.lessson_sender import send_actual_lessons
 from apscheduler.schedulers.background import BlockingScheduler
 
+print("Running scheduler")
+
 scheduler = BlockingScheduler()
 scheduler.add_job(send_actual_lessons, 'cron', hour=8, minute=1, second=0, args=(app, ))
 scheduler.start()
