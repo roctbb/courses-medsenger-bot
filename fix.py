620e51c1147b35ecbc9b2136bc50318b8aca6c16
roctbb <roctbb@gmail.com>
2023-11-27 19:05:22 +0300

    fix

--- fix.py
+++ fix.py
@@ -1,7 +1,4 @@
-contract_ids = [9451, 9701, 9731, 10010, 10011, 10012, 10017, 10022, 10033, 10039, 10045, 10046, 10047, 10060, 10073,
-                10074, 10075, 10084, 10085, 10086, 10091, 10104, 10131, 10134, 10135, 10139, 10143, 10150, 10160, 10161,
-                10177, 10183, 10185, 10188, 10195, 10196, 10201, 10205, 10215, 10216, 10248, 10255, 10276, 10277, 10278,
-                10279, 10307, 10332, 10388, 10389, 10409, 10410, 10414, 10422, 10435, 10436]
+contract_ids = [9451,10435,10436,10422,10389,10414,10409,10410,10388,10332,10307,10279,10276,10278,10277,9731]
 
 from models import *
 from logic.lessson_sender import send_initial_lessons
@@ -9,7 +6,7 @@ from datetime import timedelta
 
 with app.app_context():
     for contract_id in contract_ids:
-        enrollment = Enrollment.query.filter_by(contract_id=contract_id, course_id=4).first()
+        enrollment = Enrollment.query.filter_by(contract_id=contract_id, course_id=5).first()
 
         if enrollment:
             enrollment.created_on = enrollment.created_on + timedelta(days=21)
