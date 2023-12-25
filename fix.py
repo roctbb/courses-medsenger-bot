d6b99a0cab8e4e354e162fb9e5f0aae5b63268f5
roctbb <roctbb@gmail.com>
2023-12-25 11:45:57 +0300

    add course_id

--- fix.py
+++ fix.py
@@ -1,11 +1,15 @@
-
 from models import *
 from datetime import timedelta
 
 with app.app_context():
     for contract in Contract.query.all():
-        incomplete_enrollments = list(filter(lambda e: not e.completed, contract.enrollments))
+        if datetime.now() - timedelta(days=21) < contract.created_on:
+            continue
 
-        incomplete_enrollments.sort(key=lambda e: e.course_id)
+        incomplete_enrollments = list(filter(lambda e: not e.completed and e.created_at, contract.enrollments))
 
-        print(f"found incomplete enrollments for contract {contract.id}:", list(map(lambda ie: ie.course_id, incomplete_enrollments)))
+        if incomplete_enrollments:
+            incomplete_enrollments.sort(key=lambda e: e.course_id)
+
+            print(f"found incomplete enrollments for contract {contract.id}:",
+                  list(map(lambda ie: ie.course_id, incomplete_enrollments)))
