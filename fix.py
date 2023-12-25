744863c099e5fc5f65a5a7fe5e5a58e51be93a3b
roctbb <roctbb@gmail.com>
2023-12-25 11:49:40 +0300

    add course_id

--- fix.py
+++ fix.py
@@ -9,7 +9,18 @@ with app.app_context():
         incomplete_enrollments = list(filter(lambda e: not e.completed, contract.enrollments))
 
         if incomplete_enrollments:
+
+            print("contract id:", contract.id)
+
             incomplete_enrollments.sort(key=lambda e: e.course_id)
 
-            print(f"found incomplete enrollments for contract {contract.id}:",
+            print(f"found incomplete enrollments:",
                   list(map(lambda ie: ie.course_id, incomplete_enrollments)))
+
+            first_enrollment = incomplete_enrollments[0]
+            print("first enrollment:", first_enrollment.course_id)
+
+            last_sent_lesson = first_enrollment.sent_lessons[-1]
+            print("last sent lesson:", last_sent_lesson.to_dict())
+
+
