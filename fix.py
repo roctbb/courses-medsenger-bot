b71bbca24276e8cd8e0291746560e510eef58456
roctbb <roctbb@gmail.com>
2024-02-05 17:55:35 +0300

    Refactor contract enrollment processing
    Removed redundant checks and reorganized the code to streamline and simplify the contract enrollment processing. This update enhances the way courseids are managed, course enrollments are sorted, and how new enrollments are created and attached to contracts. The changes also ensure that empty or completed courses are correctly handled.

--- fix.py
+++ fix.py
@@ -1,54 +1,22 @@
 from models import *
 from datetime import timedelta
 
-
-def is_empty(enrollment):
-    return len(enrollment.get_sent_lessons()) == 0
-
-
 with app.app_context():
     contracts = Contract.query.all()
     contracts.sort(key=lambda contract: contract.id)
     for contract in contracts:
-        if datetime.now() - timedelta(days=21) < contract.created_on:
+        enrollments = list(sorted(contract.enrollments, key=lambda e: e.created_on))
+
+        if not enrollments or enrollments[-1].course_id < 2:
             continue
 
-        incomplete_enrollments = list(filter(lambda e: not e.completed, contract.enrollments))
+        last_enrollment = enrollments[-1]
+        last_date = last_enrollment.created_on
 
-        if incomplete_enrollments:
+        for course_id in range(last_enrollment.course_id, 8):
+            last_date += timedelta(days=21)
+            print(f"Planning to add enrollment for course {course_id} for contract {contract.id} starting at {last_date}")
+            enrollment = Enrollment(course_id=course_id, created_on=last_date, contract_id=contract.id, completed=False)
+            contract.enrollments.append(enrollment)
 
-            print("contract id:", contract.id)
-
-            incomplete_enrollments.sort(key=lambda e: e.course_id)
-
-            print(f"found incomplete enrollments:",
-                  list(map(lambda ie: ie.course_id, incomplete_enrollments)))
-
-            for index, enrollment in enumerate(incomplete_enrollments):
-                if not is_empty(enrollment):
-                    continue
-                break
-
-            last_index = max(0, index - 1)
-            print("last stopped enrollment:", last_index)
-
-            for index, enrollment in enumerate(incomplete_enrollments):
-                if index < last_index:
-                    enrollment.completed = True
-                    print(f"Will mark course {enrollment.course_id} as completed")
-
-                elif index == last_index:
-                    sent_count = len(enrollment.get_sent_lessons())
-                    start_date = datetime.now() - timedelta(days=sent_count)
-                    enrollment.created_on = start_date
-                    print(f"Will set start date to {start_date} for course {enrollment.course_id}")
-
-                else:
-                    start_date += timedelta(days=21)
-                    enrollment.created_on = start_date
-                    print(f"Will set start date to {start_date} for course {enrollment.course_id}")
-
-            print()
-            print()
-
-    db.session.commit()
+    # db.session.commit()
