7e5c8c9303a22ee927457b7d2a8c99ba45924a97
roctbb <roctbb@gmail.com>
2024-02-07 14:45:15 +0300

    prepare for emperor

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo pip3 install -r requirements.txt
+sudo cp courses.ini /etc/uwsgi/apps/
 sudo cp agents_courses.conf /etc/supervisor/conf.d/
 sudo cp agents_courses_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
